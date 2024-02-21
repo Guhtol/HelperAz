d4ef788f00ad378a7a4c431b5ba8716e7c272d25
guhtol <programador.bueno@gmail.com>
2024-02-21 05:52:40 -0300

    feature: adicionado variaveis de ambiente

--- src/Presentation/Program.cs
+++ src/Presentation/Program.cs
@@ -1,5 +1,18 @@
 ﻿using Application.Command;
 using Infrastructure;
+using Microsoft.Extensions.Configuration;
 
-var sendRequest = new SendRequestToAzureFunction(new ReadJsons());
+IConfiguration configuration = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json")
+                            .AddEnvironmentVariables()
+                            .Build();
+
+var azureFunctionUrl = configuration.GetValue<string>("AzureFunctionUrl") ?? string.Empty;
+if (string.IsNullOrEmpty(azureFunctionUrl))
+{
+    Console.WriteLine("Configuração AzureFunctionUrl não encontrada no arquivo appsetings.json");
+    return;
+}
+
+var sendRequest = new SendRequestToAzureFunction(new ReadJsons(), azureFunctionUrl);
 await sendRequest.ExecuteCommand();
